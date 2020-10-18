b361ab78c371961362f7d740ff9f3a77c4d7ed52
xmmt <mkmax472@gmail.com>
2020-10-18 21:50:54 +0300

    created cmake project

---
+++ experimental-3d-engine/experimental-3d-engine/experimental-3d-engine.cpp
@@ -0,0 +1,12 @@
+﻿// experimental-3d-engine.cpp: определяет точку входа для приложения.
+//
+
+#include "experimental-3d-engine.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
