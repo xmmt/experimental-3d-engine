4b81a881bb8e126a8caa6d3b6eaa110ea0cf7578
xmmt <mkmax472@gmail.com>
2020-10-21 00:52:55 +0300

    glfw

--- experimental-3d-engine/experimental-3d-engine/experimental-3d-engine.cpp
+++ experimental-3d-engine/experimental-3d-engine/experimental-3d-engine.cpp
@@ -1,239 +1,146 @@
-﻿/* We want the core profile, so we include GL/glcorearb.h. When including that, then
-   GL/gl.h should not be included.
-
-   If using compatibility profile, the GL/gl.h and GL/glext.h need to be included.
-
-   GL/wglext.h gives WGL extensions.
-
-   Note that Windows.h needs to be included before them. */
-
-#include <cstdio>
-#define GLAD_GL_IMPLEMENTATION
+﻿#define GLAD_GL_IMPLEMENTATION
 #define GLAD_WGL_IMPLEMENTATION
 #include <glad/wgl.h>
+#define GLFW_INCLUDE_NONE
+#include <GLFW/glfw3.h>
 
-LRESULT CALLBACK window_procedure(HWND, UINT, WPARAM, LPARAM);
-void* get_proc(const char*);
+#include <stdlib.h>
+#include <stdio.h>
+#include <thread>
 
-/* gl_module is for opening the DLL, and the quit flag is here to prevent
-   quitting when recreating the window (see the window_procedure function) */
+#include "glfw/deps/linmath.h"
 
-HMODULE gl_module;
-bool quit = false;
+static const struct
+{
+    float x, y;
+    float r, g, b;
+} vertices[3] = {
+    { -0.6f, -0.4f, 1.f, 0.f, 0.f },
+    { 0.6f, -0.4f, 0.f, 1.f, 0.f },
+    { 0.f, 0.6f, 0.f, 0.f, 1.f }
+};
 
-/* OpenGL function declarations. In practice, we would put these in a
-   separate header file and add "extern" in front, so that we can use them
-   anywhere after loading them only once. */
+static const char* vertex_shader_text = "#version 110\n"
+                                        "uniform mat4 MVP;\n"
+                                        "attribute vec3 vCol;\n"
+                                        "attribute vec2 vPos;\n"
+                                        "varying vec3 color;\n"
+                                        "void main()\n"
+                                        "{\n"
+                                        "    gl_Position = MVP * vec4(vPos, 0.0, 1.0);\n"
+                                        "    color = vCol;\n"
+                                        "}\n";
 
-//PFNWGLGETEXTENSIONSSTRINGARBPROC wglGetExtensionsStringARB;
-//PFNWGLCHOOSEPIXELFORMATARBPROC wglChoosePixelFormatARB;
-//PFNWGLCREATECONTEXTATTRIBSARBPROC wglCreateContextAttribsARB;
-//PFNGLGETSTRINGPROC glGetString;
+static const char* fragment_shader_text = "#version 110\n"
+                                          "varying vec3 color;\n"
+                                          "void main()\n"
+                                          "{\n"
+                                          "    gl_FragColor = vec4(color, 1.0);\n"
+                                          "}\n";
 
-void draw();
-
-int WINAPI WinMain(HINSTANCE instance_handle, HINSTANCE prev_instance_handle, PSTR cmd_line, int cmd_show) {
-    /* REGISTER WINDOW */
-    WNDCLASS window_class;
-
-    // Clear all structure fields to zero first
-    ZeroMemory(&window_class, sizeof(window_class));
-
-    // Define fields we need (others will be zero)
-    window_class.style = CS_HREDRAW | CS_VREDRAW | CS_OWNDC;
-    window_class.lpfnWndProc = window_procedure;
-    window_class.hInstance = instance_handle;
-    window_class.lpszClassName = TEXT("OPENGL_WINDOW");
-
-    // Give our class to Windows
-    RegisterClass(&window_class);
-    /* *************** */
-
-    /* CREATE WINDOW */
-    HWND window_handle = CreateWindowEx(WS_EX_OVERLAPPEDWINDOW,
-      TEXT("OPENGL_WINDOW"),
-      TEXT("OpenGL window"),
-      WS_OVERLAPPEDWINDOW,
-      0, 0,
-      800, 600,
-      NULL,
-      NULL,
-      instance_handle,
-      NULL);
-
-    HDC dc = GetDC(window_handle);
-
-    ShowWindow(window_handle, SW_SHOW);
-    /* ************* */
-
-    /* PIXEL FORMAT */
-    PIXELFORMATDESCRIPTOR descriptor;
-
-    // Clear all structure fields to zero first
-    ZeroMemory(&descriptor, sizeof(descriptor));
-
-    // Describe our pixel format
-    descriptor.nSize = sizeof(descriptor);
-    descriptor.nVersion = 1;
-    descriptor.dwFlags = PFD_DRAW_TO_WINDOW | PFD_DRAW_TO_BITMAP | PFD_SUPPORT_OPENGL | PFD_GENERIC_ACCELERATED | PFD_DOUBLEBUFFER | PFD_SWAP_LAYER_BUFFERS;
-    descriptor.iPixelType = PFD_TYPE_RGBA;
-    descriptor.cColorBits = 32;
-    descriptor.cRedBits = 8;
-    descriptor.cGreenBits = 8;
-    descriptor.cBlueBits = 8;
-    descriptor.cAlphaBits = 8;
-    descriptor.cDepthBits = 32;
-    descriptor.cStencilBits = 8;
-
-    // Ask for a similar supported format and set it
-    int pixel_format = ChoosePixelFormat(dc, &descriptor);
-    SetPixelFormat(dc, pixel_format, &descriptor);
-    /* *********************** */
-
-    /* RENDERING CONTEXT */
-    HGLRC rc = wglCreateContext(dc);
-    wglMakeCurrent(dc, rc);
-    /* ***************** */
-
-    /* LOAD FUNCTIONS (should probably be put in a separate procedure) */
-    gl_module = LoadLibrary(TEXT("opengl32.dll"));
-
-    wglGetExtensionsStringARB = (PFNWGLGETEXTENSIONSSTRINGARBPROC)get_proc("wglGetExtensionsStringARB");
-    wglChoosePixelFormatARB = (PFNWGLCHOOSEPIXELFORMATARBPROC)get_proc("wglChoosePixelFormatARB");
-    wglCreateContextAttribsARB = (PFNWGLCREATECONTEXTATTRIBSARBPROC)get_proc("wglCreateContextAttribsARB");
-    //glGetString = (PFNGLGETSTRINGPROC)get_proc("glGetString");
-    auto r1 = gladLoadGL((GLADloadfunc)get_proc);
-    auto r2 = gladLoadWGL(dc, (GLADloadfunc)get_proc);
-
-    FreeLibrary(gl_module);
-    /* ************** */
-
-    /* PRINT VERSION */
-    const GLubyte* version = glGetString(GL_VERSION);
-    printf("%s\n", version);
-    fflush(stdout);
-    /* ******* */
-
-    /* NEW PIXEL FORMAT*/
-    int pixel_format_arb;
-    UINT pixel_formats_found;
-
-    int pixel_attributes[] = {
-        WGL_SUPPORT_OPENGL_ARB, 1,
-        WGL_DRAW_TO_WINDOW_ARB, 1,
-        WGL_DRAW_TO_BITMAP_ARB, 1,
-        WGL_DOUBLE_BUFFER_ARB, 1,
-        WGL_SWAP_LAYER_BUFFERS_ARB, 1,
-        WGL_COLOR_BITS_ARB, 32,
-        WGL_RED_BITS_ARB, 8,
-        WGL_GREEN_BITS_ARB, 8,
-        WGL_BLUE_BITS_ARB, 8,
-        WGL_ALPHA_BITS_ARB, 8,
-        WGL_DEPTH_BITS_ARB, 32,
-        WGL_STENCIL_BITS_ARB, 8,
-        WGL_ACCELERATION_ARB, WGL_FULL_ACCELERATION_ARB,
-        WGL_PIXEL_TYPE_ARB, WGL_TYPE_RGBA_ARB,
-        0
-    };
-
-    BOOL result = wglChoosePixelFormatARB(dc, pixel_attributes, NULL, 1, &pixel_format_arb, &pixel_formats_found);
-
-    if (!result) {
-        printf("Could not find pixel format\n");
-        fflush(stdout);
-        return 0;
-    }
-    /* **************** */
-
-    /* RECREATE WINDOW */
-    wglMakeCurrent(dc, NULL);
-    wglDeleteContext(rc);
-    ReleaseDC(window_handle, dc);
-    DestroyWindow(window_handle);
-
-    window_handle = CreateWindowEx(WS_EX_OVERLAPPEDWINDOW,
-      TEXT("OPENGL_WINDOW"),
-      TEXT("OpenGL window"),
-      WS_OVERLAPPEDWINDOW,
-      0, 0,
-      800, 600,
-      NULL,
-      NULL,
-      instance_handle,
-      NULL);
-
-    dc = GetDC(window_handle);
-
-    ShowWindow(window_handle, SW_SHOW);
-    /* *************** */
-
-    /* NEW CONTEXT */
-    GLint context_attributes[] = {
-        WGL_CONTEXT_MAJOR_VERSION_ARB, 4,
-        WGL_CONTEXT_MINOR_VERSION_ARB, 6,
-        WGL_CONTEXT_PROFILE_MASK_ARB, WGL_CONTEXT_CORE_PROFILE_BIT_ARB,
-        0
-    };
-
-    rc = wglCreateContextAttribsARB(dc, 0, context_attributes);
-    wglMakeCurrent(dc, rc);
-    /* *********** */
-
-    //glViewport(0, 0, 800, 600);
-
-    /* EVENT PUMP */
-    MSG msg;
-
-    while (true) {
-        if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)) {
-            if (msg.message == WM_QUIT)
-                break;
-
-            TranslateMessage(&msg);
-            DispatchMessage(&msg);
-        }
-
-        draw();
-
-        SwapBuffers(dc);
-    }
-    /* ********** */
-
-    return 0;
+static void error_callback(int error, const char* description) {
+    fprintf(stderr, "Error: %s\n", description);
 }
 
-// Procedure that processes window events
-LRESULT CALLBACK window_procedure(HWND window_handle, UINT message, WPARAM param_w, LPARAM param_l) {
-    /* When destroying the dummy window, WM_DESTROY message is going to be sent,
-       but we don't want to quit the application then, and that is controlled by
-       the quit flag. */
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods) {
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
+}
 
-    switch (message) {
-    case WM_DESTROY:
-        if (!quit)
-            quit = true;
-        else
-            PostQuitMessage(0);
-        return 0;
+int main(void) {
+    GLFWwindow* window;
+    GLuint vertex_buffer, vertex_shader, fragment_shader, program;
+    GLint mvp_location, vpos_location, vcol_location;
+
+    glfwSetErrorCallback(error_callback);
+
+    if (!glfwInit())
+        exit(EXIT_FAILURE);
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+
+    window = glfwCreateWindow(640, 480, "Simple example", NULL, NULL);
+    if (!window) {
+        glfwTerminate();
+        exit(EXIT_FAILURE);
     }
 
-    return DefWindowProc(window_handle, message, param_w, param_l);
-}
+    glfwSetKeyCallback(window, key_callback);
 
-/* A procedure for getting OpenGL functions and OpenGL or WGL extensions.
-   When looking for OpenGL 1.2 and above, or extensions, it uses wglGetProcAddress,
-   otherwise it falls back to GetProcAddress. */
-void* get_proc(const char* proc_name) {
-    void* proc = (void*)wglGetProcAddress(proc_name);
-    if (!proc)
-        proc = (void*)GetProcAddress(gl_module, proc_name);
+    std::thread thr([&]
+    {
 
-    return proc;
-}
+    glfwMakeContextCurrent(window);
+    gladLoadGL(glfwGetProcAddress);
+    gladLoadWGL((HDC)window, glfwGetProcAddress);
+    glfwSwapInterval(1);
 
-void draw() {
-    glViewport(0, 0, 800, 600);
-    glClearColor(0.5f, 0.0f, 0.0f, 0.5f);
-    glClear(GL_COLOR_BUFFER_BIT);
-    glFlush();
+    // NOTE: OpenGL error checks have been omitted for brevity
+
+    glGenBuffers(1, &vertex_buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertex_shader, 1, &vertex_shader_text, NULL);
+    glCompileShader(vertex_shader);
+
+    fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragment_shader, 1, &fragment_shader_text, NULL);
+    glCompileShader(fragment_shader);
+
+    program = glCreateProgram();
+    glAttachShader(program, vertex_shader);
+    glAttachShader(program, fragment_shader);
+    glLinkProgram(program);
+
+    mvp_location = glGetUniformLocation(program, "MVP");
+    vpos_location = glGetAttribLocation(program, "vPos");
+    vcol_location = glGetAttribLocation(program, "vCol");
+
+    glEnableVertexAttribArray(vpos_location);
+    glVertexAttribPointer(vpos_location, 2, GL_FLOAT, GL_FALSE,
+      sizeof(vertices[0]), (void*)0);
+    glEnableVertexAttribArray(vcol_location);
+    glVertexAttribPointer(vcol_location, 3, GL_FLOAT, GL_FALSE,
+      sizeof(vertices[0]), (void*)(sizeof(float) * 2));
+
+    while (!glfwWindowShouldClose(window)) {
+        float ratio;
+        int width, height;
+        mat4x4 m, p, mvp;
+
+        glfwGetFramebufferSize(window, &width, &height);
+        ratio = width / (float)height;
+
+        glViewport(0, 0, width, height);
+        glClearColor(0.5f, 0.f, 0.f, 1.f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        mat4x4_identity(m);
+        mat4x4_rotate_Z(m, m, (float)glfwGetTime());
+        mat4x4_ortho(p, -ratio, ratio, -1.f, 1.f, 1.f, -1.f);
+        mat4x4_mul(mvp, p, m);
+
+        glUseProgram(program);
+        glUniformMatrix4fv(mvp_location, 1, GL_FALSE, (const GLfloat*)mvp);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        glfwSwapBuffers(window);
+        //glfwPollEvents();
+    }
+
+    });
+
+    while (!glfwWindowShouldClose(window)) {
+        //std::this_thread::yield();
+        //glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+
+    glfwTerminate();
+    exit(EXIT_SUCCESS);
 }
