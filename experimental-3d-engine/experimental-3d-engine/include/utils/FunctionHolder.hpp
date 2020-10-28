92d1762fbed6a4af97e69bb5a879dcbdda8bdcc1
Vladislav Grigoriev <vvllaadd99@yandex.ru>
2020-10-29 02:06:46 +0300

    refactor function; make copy default, optimize copies/moves, make forwarding correct

--- experimental-3d-engine/experimental-3d-engine/include/utils/FunctionHolder.hpp
+++ experimental-3d-engine/experimental-3d-engine/include/utils/FunctionHolder.hpp
@@ -10,57 +10,40 @@ class Function;
 template <typename ReturnType, typename... Args>
 class Function<ReturnType(Args...)> {
 private:
-    class FunctionHolderBase {
-    public:
-        FunctionHolderBase() = default;
-        FunctionHolderBase(FunctionHolderBase const&) = delete;
-        FunctionHolderBase(FunctionHolderBase&&) = delete;
-        void operator=(FunctionHolderBase const&) = delete;
-        void operator=(FunctionHolderBase&&) = delete;
+    struct FunctionHolderBase {
         virtual ~FunctionHolderBase() = default;
-
         virtual ReturnType invoke(Args&&...) = 0;
     };
     using invoker_t = std::unique_ptr<FunctionHolderBase>;
 
     template <typename F>
-    class FunctionHolder final : public FunctionHolderBase {
-    public:
+    struct FunctionHolder final : public FunctionHolderBase {
+        F function_;
+        FunctionHolder(const F& f)
+            : FunctionHolderBase()
+            , function_(f) {
+        }
         FunctionHolder(F&& f)
             : FunctionHolderBase()
-            , function_(std::forward<F>(f)) {
+            , function_(std::move(f)) {
         }
-        void operator=(FunctionHolder const&) = delete;
-        void operator=(FunctionHolder&&) = delete;
-        ~FunctionHolder() = default;
 
-        ReturnType invoke(Args&&... args) final {
+        inline ReturnType invoke(Args&&... args) final {
             return function_(std::forward<Args>(args)...);
         }
-
-    private:
-        F function_;
     };
 
-    invoker_t invoker_;
+    mutable invoker_t invoker_;
 
 public:
     Function() = default;
-    Function(Function const&) = delete;
-    Function(Function&&) = default;
-    Function& operator=(Function const&) = delete;
-    Function& operator=(Function&&) = default;
 
-    template <typename F>
-    Function(F const& f)
-        : invoker_(std::make_unique<FunctionHolder<F>>(f)) {
-    }
     template <typename F>
     Function(F&& f)
-        : invoker_(std::make_unique<FunctionHolder<F>>(std::forward<F>(f))) {
+        : invoker_(std::make_unique<FunctionHolder<typename std::decay<F>::type>>(std::forward<F>(f))) {
     }
 
-    inline ReturnType operator()(Args... args) {
+    inline ReturnType operator()(Args... args) const {
         return invoker_->invoke(std::forward<Args>(args)...);
     }
     inline operator bool() const {
